6e715e320b271844afbf3eb336bfea9996815b59
dgarcia360 <dgarcia360@outlook.com>
2020-05-31 09:43:09 +0100

    Updated dir

--- docs/_utils/deploy.sh
+++ docs/_utils/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" --branch gh-pages --single-branch gh-pages
-cp -r docs/_build/html/* gh-pages/
+cp -r docs/_build/dirhtml/* gh-pages/
 cd gh-pages
 git config --local user.email "action@scylladb.com"
 git config --local user.name "GitHub Action"
