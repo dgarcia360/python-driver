ef14e55f63ce4d7d0fb8d67745f784daecb9f911
dgarcia360 <dgarcia360@outlook.com>
2020-06-03 17:52:12 +0100

    disable custom config

--- docs/conf.py
+++ docs/conf.py
@@ -231,12 +231,12 @@ man_pages = [
 
 # -- Options for multiversion --------------------------------------------
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'^.*$'
+#smv_tag_whitelist = r'^.*$'
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^master$"
+#smv_branch_whitelist = r"^master$"
 # Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = r"^origin$"
+#smv_remote_whitelist = r"^origin$"
 # Pattern for released versions
-smv_released_pattern = r'^tags/.*$'
+#smv_released_pattern = r'^tags/.*$'
 # Format for versioned output directories inside the build directory
-smv_outputdir_format = '{ref.name}'
+#smv_outputdir_format = '{ref.name}'
